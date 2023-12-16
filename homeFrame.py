0179745ca3ae773677e44e16159d662a66ecb739
CyberBow <70140026+SalimRK@users.noreply.github.com>
2023-12-16 22:48:34 +0200

    Update homeFrame.py

--- homeFrame.py
+++ homeFrame.py
@@ -2,16 +2,16 @@ import customtkinter as ctk
 
 
 class HomeFrame(ctk.CTkFrame):
-
-    text = """
-    This project is an app for streaming movies and tv shows for free with no fees or ads
-    author: salim rizk
-    github: 
-    
-    
-    """
     def __init__(self, master, **kwargs):
         super().__init__(master, **kwargs)
-        self.label = ctk.CTkLabel(self, text="")
-        self.label.grid(row=1, column=0, padx=10, pady=10)
+        self.text = """
+        This project is an app for streaming movies and tv shows for free with no fees or ads
 
+        Author: salim rizk
+        Github: https://github.com/SalimRK/
+        Gmail: rk@salimrk@gmail.com
+        Number: PRIVET 
+        linkedin: https://www.linkedin.com/in/salim-rizk/
+        """
+        self.label = ctk.CTkLabel(self, text=self.text)
+        self.label.grid(row=0, column=0, padx=10, pady=10)
